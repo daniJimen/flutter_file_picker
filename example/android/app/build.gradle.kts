3867781b07a3306077c88894778edc1e8140fdd4
Daniel Jimenez <122808735+daniJimen@users.noreply.github.com>
2025-11-11 10:26:35 +0100

    feat: update ndk version to 27.0.12077973

--- example/android/app/build.gradle.kts
+++ example/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.mr.flutter.plugin.filepicker.file_picker_example"
     compileSdk = 36
-    ndkVersion = 26.1.10909125
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
